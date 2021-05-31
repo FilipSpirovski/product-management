537b7acf6ede247db69bb3837f16f8073ebe19f7
FilipSpirovski <spirovskii.filipp@gmail.com>
2021-05-31 12:38:29 +0200

    binding with interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { Component } from "@angular/core";
   `
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management'
+  pageTitle: string = 'Acme Product Management';
 }
