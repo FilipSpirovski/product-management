5e15a619ec74e61e8ece2cbc6728d8396fcac67f
FilipSpirovski <spirovskii.filipp@gmail.com>
2021-06-02 12:37:40 +0200

    passing route parameters and placing route guards

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
+    const param = this.route.snapshot.paramMap.get('id');
+    
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+    }
   }
 
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
